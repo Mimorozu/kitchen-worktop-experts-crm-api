d94f5f5f1ac52bca3be4c2d53357501628469834
Ethan <mimorozu@gmail.com>
2026-08-17 21:34:41 +0100

    remove diagnostic logging now that cloudinary upload is confirmed working

--- src/cloudinary.js
+++ src/cloudinary.js
@@ -1,14 +1,5 @@
 const cloudinary = require('cloudinary').v2
 
-console.log('Cloudinary env check:', {
-  cloud_name_present: !!process.env.CLOUDINARY_CLOUD_NAME,
-  cloud_name_length: (process.env.CLOUDINARY_CLOUD_NAME || '').length,
-  api_key_present: !!process.env.CLOUDINARY_API_KEY,
-  api_key_length: (process.env.CLOUDINARY_API_KEY || '').length,
-  api_secret_present: !!process.env.CLOUDINARY_API_SECRET,
-  api_secret_length: (process.env.CLOUDINARY_API_SECRET || '').length
-})
-
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
